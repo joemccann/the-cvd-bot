987263bae6c8c69e50747059b78934583852c6c7
joemccann <joseph.isaac@gmail.com>
2020-03-10 19:06:40 -0700

    wrapped in a try/catch because dep doesn't bubble properly

--- bot/index.js
+++ bot/index.js
@@ -1,43 +1,54 @@
 const bot = require('./src/')
 
 module.exports = async function (context, request) {
-  context.log('Azure Function Sanity ✓')
-
-  const { rawBody = null } = request
-
-  let update = null
-
   //
-  // By default anyway and we must leave it as Telegram will retry forever.
+  // tgfancy doens't bubble errors property so we have to wrap in try/catch
   //
-  const status = 200
-
   try {
-    update = JSON.parse(rawBody)
+    context.log('Azure Function Sanity ✓')
+
+    const { rawBody = null } = request
+
+    let update = null
+
+    //
+    // By default anyway and we must leave it as Telegram will retry forever.
+    //
+    const status = 200
+
+    try {
+      update = JSON.parse(rawBody)
+    } catch (err) {
+      context.res = {
+        body: err,
+        status
+      }
+
+      return { err }
+    }
+
+    const { err, data: body } = await bot(update)
+
+    if (err) {
+      context.res = {
+        body: err.message,
+        status
+      }
+    }
+
+    context.res = {
+      body,
+      status
+    }
+
+    const { req, res } = context
+
+    return { data: res, status: res.status, req }
   } catch (err) {
     context.res = {
-      body: err,
-      status
-    }
-
-    return { err }
-  }
-
-  const { err, data: body } = await bot(update)
-
-  if (err) {
-    context.res = {
       body: err.message,
-      status
+      status: 200
     }
+    return { err: err.message }
   }
-
-  context.res = {
-    body,
-    status
-  }
-
-  const { req, res } = context
-
-  return { data: res, status: res.status, req }
 }
